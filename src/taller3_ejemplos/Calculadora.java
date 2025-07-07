f31cb5fdc77259173a6677e8eda4e0f90e921b25
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-06 20:07:17 -0500

    feat: emiro puello - ejemplo método static sumar en clase Calculadora del Taller 3

---
+++ src/taller3_ejemplos/Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller3_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Calculadora {
+   
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+} 
+
