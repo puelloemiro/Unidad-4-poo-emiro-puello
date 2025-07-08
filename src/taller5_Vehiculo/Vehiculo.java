f2e0dc33158d5b6823dab883ce66a329529bfd04
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:40:54 -0500

    feat: Emiro Puello - Vehiculo.java - ejercicio taller 5

---
+++ src/taller5_Vehiculo/Vehiculo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_Vehiculo;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Vehiculo {
+    
+    String tipo = "Motocicleta";
+
+    void mostrarTipo() {
+        System.out.println("Tipo de vehículo: " + tipo);
+    }
+}  
+
