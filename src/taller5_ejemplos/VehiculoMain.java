2b72358281538a23536f124b13262ae6ae7f437c
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:08:37 -0500

    feat: Emiro Puello - VehiculoMain.java - ejemplo taller 5

---
+++ src/taller5_ejemplos/VehiculoMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_ejemplos;
+import taller5_ejemplos.Vehiculo;
+/**
+ *
+ * @author usuario1
+ */
+public class VehiculoMain {
+    
+    public static void main (String[] args){
+        Vehiculo v = new Vehiculo ("Toyota"); // error de compilacion: Vehiculo no es visible
+    }
+}
