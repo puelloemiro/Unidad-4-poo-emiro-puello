42811517a0a886576e3d610d87ba1cb09662f78d
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:22:15 -0500

    feat: Emiro Puello - PruebaVehiculo.java - ejercicio taller 5

---
+++ src/taller5_prueba/PruebaVehiculo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_prueba;
+
+/**
+ *
+ * @author usuario1
+ */
+
+
+public class PruebaVehiculo {
+    public static void main(String[] args) {
+       // Moto moto = new Moto(); // Error: Moto tiene acceso default en otro paquete
+
+       // Moto.encender();        //  No se puede acceder si Moto no es pública
+
+        // moto.mostrarTipo();  //Error: método con acceso default en clase padre Vehiculo
+        // System.out.println(moto.tipo); //Error: atributo con acceso default
+    }
+}
