b93fc296de28393aefc72ac4fd3de1ec08a87c2b
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 16:46:19 -0500

    feat: Emiro Puello - Interna.java - ejercicio taller 4

---
+++ src/taller4_ejercicios/Interna.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Interna {
+    
+     private int dato = 42;
+}
+
