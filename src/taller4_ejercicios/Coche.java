76cbd24371b1999701211780e9206e180e72c2d9
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 16:41:19 -0500

    feat: Emiro Puello - Coche.java - ejercicio taller 4

---
+++ src/taller4_ejercicios/Coche.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Coche {
+    
+    private String marca;
+    private String modelo;
+    private int velocidadMaxima;
+
+    public Coche(String marca, String modelo, int velocidadMaxima) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void acelerar(int incremento) {
+        if (incremento > 0) {
+            velocidadMaxima += incremento;
+            System.out.println("Nueva velocidad máxima: " + velocidadMaxima);
+        } else {
+            System.out.println("El incremento debe ser mayor que cero.");
+        }
+    }
+}
