5173ebb7a7d82dedd7c656ee3f097e2b0c4c316f
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:20:04 -0500

    feat: Emiro Puello - Persona.java - ejercicio taller 5

---
+++ src/taller5_ejercicios/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+class Persona {
+    private String nombre; // Atributo privado
+    int edad;              // Atributo con acceso de paquete (default)
+
+    // Métodos getter y setter para el atributo privado
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
