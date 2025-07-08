66c91d67cede2c75dc7d64601be2416a0dfebc43
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 16:37:01 -0500

    feat: Emiro Puello - Persona.java - ejemplo taller 4

---
+++ src/taller4_ejemplos/Persona.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller4_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Métodos públicos para acceder a los atributos privados
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+    }
