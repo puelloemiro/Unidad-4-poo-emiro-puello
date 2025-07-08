d01e6369eefbd767d743b5b5b7aa19eec546a1ce
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:06:18 -0500

    feat: Emiro Puello - Producto.java - ejemplo taller 5

---
+++ src/taller5_ejemplos/Producto.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Producto {
+    String nombre; // Atributo de paquete (default)
+double precio; // Atributo de paquete (default)
+Producto(String nombre, double precio) { // Constructor de paquete
+this.nombre = nombre;
+this.precio = precio;
+//void mostrarInfo() { // Método de paquete 
+
+    System.out.println("Nombre:  + nombre + ");
+}
+}
+
