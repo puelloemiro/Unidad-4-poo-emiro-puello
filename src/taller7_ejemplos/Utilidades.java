dcee6d49d0115a5db356479624506d4c7055a5f6
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 18:36:09 -0500

    feat: Emiro Puello - Utilidades.java - ejemplo taller 7

---
+++ src/taller7_ejemplos/Utilidades.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller7_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Utilidades {
+   
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
+
+
