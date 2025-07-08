a73b3ecfb1fe4d70fac15c1a667f293a6e5220f6
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:50:42 -0500

    feat: Emiro Puello - MainAnimal.java - ejemplo taller 6

---
+++ src/taller6_ejemplos/MainAnimal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller6_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainAnimal {
+
+ public static void main(String[] args) {
+        Perro perro = new Perro("Canino");
+        perro.emitirSonido(); // Llama al método sobrescrito
+    }
+ 
+}
