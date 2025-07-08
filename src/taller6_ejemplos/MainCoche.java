6033d41c6b079bee91e7b790c9d174ea5ae85ff6
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:51:17 -0500

    feat: Emiro Puello - MainCoche.java - ejemplo taller 6

---
+++ src/taller6_ejemplos/MainCoche.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller6_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainCoche {
+    
+    public static void main(String[] args) {
+        Coche miCoche = new Coche("Toyota", 180.5, 4);
+        miCoche.mostrarInformacion();
+    }
+}
