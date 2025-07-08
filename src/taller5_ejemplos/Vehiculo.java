4b2b662eaa52f244becd39cf48fa6713ba2e0608
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:08:28 -0500

    feat: Emiro Puello - Vehiculo.java - ejemplo taller 5

---
+++ src/taller5_ejemplos/Vehiculo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+
+//paquete: paqueteA
+public class Vehiculo {
+    
+    String marca;
+    
+    Vehiculo(String marca) {
+        this.marca = marca;
+    }
+   
+    
+    
+    }
+    
