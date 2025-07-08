88d5579e77a9ad3b37cd20af63a0759ed65ec213
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 17:41:05 -0500

    feat: Emiro Puello - Moto.java - ejercicio taller 5

---
+++ src/taller5/Vehiculo/Moto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5.Vehiculo;
+
+import taller5_Vehiculo.Vehiculo;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Moto extends Vehiculo {
+    void encender() {
+        System.out.println("La moto ha sido encendida.");
+    }
+}
+
+    
+
