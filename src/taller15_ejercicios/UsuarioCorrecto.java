0385e35de9bbe7f19d93e7db8d3f889f3d4ce944
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:50:00 -0500

    feat: Emiro Puello - UsuarioCorrecto.java - ejercicio taller 15

---
+++ src/taller15_ejercicios/UsuarioCorrecto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller15_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class UsuarioCorrecto {
+  
+    private String nombreUsuario;
+    private String contraseña;
+
+    public UsuarioCorrecto(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+    
+}
