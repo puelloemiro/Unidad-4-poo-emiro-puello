b71a3390fd9c58f0a8cf1b7a41d7b62daff04512
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:05:05 -0500

    feat: Emiro Puello - MainErroresSuper.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/MainErroresSuper.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainErroresSuper {
+    
+    public static void main(String[] args) {
+        ClaseDerivada obj = new ClaseDerivada();
+        obj.mostrarMensaje();
+    }
+    
+}
