a20da6056e2bcb6efdd1410fd97f18331429a1af
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:35:49 -0500

    feat: Emiro Puello - Ejemplo3.java - ejemplo taller 11

---
+++ src/taller11_ejemplos/Ejemplo3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Ejemplo3 {
+    
+    // Clase abstracta
+public abstract class Vehiculo {
+public abstract void conducir(); // Método abstracto
+}
+
+
+// Clase derivada incorrecta
+//public class Coche extends Vehiculo {
+// Error de compilación: La clase Coche debe implementar el método abstracto conducir()
+    }
+//}
