a1e9612d3ce8c74cf913fc044c57597784ed2f7d
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:48:43 -0500

    feat: Emiro Puello - LibroPersistencia.java - ejercicio taller 15

---
+++ src/taller15_ejercicios/LibroPersistencia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller15_ejercicios;
+
+import taller15_ejercicios.LibroCorrecto.Libro;
+
+/**
+ *
+ * @author usuario1
+ */
+public class LibroPersistencia {
+    
+    public void guardar(Libro libro) {
+        System.out.println("Guardando '" + libro.getTitulo() + "' en archivo...");
+    }    
+}
