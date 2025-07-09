97ce711974fbc054fc754b70af222ed22180a371
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:14:41 -0500

    feat: Emiro Puello - ClaseC.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/ClaseC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+
+Esto provoca un error de compilación:
+Java no permite herencia múltiple entre clases.
+
+
+// public class ClaseC extends ClaseA, ClaseB { // Error
+//     public void metodoC() {
+//         System.out.println("Método C");
+//     }
+// }
+*/
