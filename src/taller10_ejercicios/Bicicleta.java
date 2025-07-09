1332877edcba77c03bf55c4e5397a076f41f4f29
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:23:24 -0500

    feat: Emiro Puello - Bicicleta.java - ejercicio taller 10

---
+++ src/taller10_ejercicios/Bicicleta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller10_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Bicicleta extends Vehiculo {
+    
+@Override
+    
+    public void moverse() {
+        System.out.println("La bicicleta avanza pedaleando.");
+    }    
+}
