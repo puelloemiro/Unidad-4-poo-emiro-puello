66a7439f9421846936eb11030881cf428fe70455
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:36:58 -0500

    feat: Emiro Puello - FiguraInvalida.java - ejercicio taller 11

---
+++ src/taller11_ejercicios/FiguraInvalida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public abstract class FiguraInvalida {
+    
+    public abstract double calcularArea();
+
+}    
+
