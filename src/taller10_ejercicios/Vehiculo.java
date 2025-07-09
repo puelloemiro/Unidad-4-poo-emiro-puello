cdea62f0434e82fa3bf2b09f809dccf0696f3a47
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:32:10 -0500

    feat: Emiro Puello - Vehiculo.java - ejercicio taller 10

---
+++ src/taller10_ejercicios/Vehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller10_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Vehiculo {
+    
+    public void moverse() {
+        System.out.println("El vehículo se está moviendo.");
+    }
+    
+}
