5fc0cc8c392dfcba0e125e1eff1b7f6c0cce87db
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:40:12 -0500

    feat: Emiro Puello - MainPato.java - ejercicio taller 13

---
+++ src/taller13_ejercicios/MainPato.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller13_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainPato {
+    
+    public static void main(String[] args) {
+        Pato pato = new Pato();
+
+        pato.volar(); // Método de Volador
+        pato.nadar(); // Método de Nadador
+    }
+}
