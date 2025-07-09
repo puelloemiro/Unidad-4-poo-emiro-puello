49d610cc45728ffac5bddd7d81c9e41903471b9a
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:42:45 -0500

    feat: Emiro Puello - Animal.java - ejercicio taller 14

---
+++ src/taller14_ejercicios/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller14_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Animal {
+    
+    public void hacerSonido() {
+        System.out.println("El animal hace un sonido.");
+    }
+    
+}
