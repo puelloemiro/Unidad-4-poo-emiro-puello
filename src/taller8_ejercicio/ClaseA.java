2d27ba129a2763094a1a816713947b728b0f2bc9
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:14:27 -0500

    feat: Emiro Puello - ClaseA.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/ClaseA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class ClaseA {
+    
+    public void metodoA() {
+        System.out.println("Método A");
+    }
+}
