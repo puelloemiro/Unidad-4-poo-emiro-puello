80d1e34e204cf0e28af1c2d44a70b7dc48584512
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:40:50 -0500

    feat: Emiro Puello - SuperClase.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/SuperClase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class SuperClase {
+    
+    private String mensaje = "Hola desde SuperClase";
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    
+}
