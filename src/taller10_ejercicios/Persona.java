04d1aa93b7382472ef472f9501f28b46990858f1
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:31:46 -0500

    feat: Emiro Puello - Persona.java - ejercicio taller 10

---
+++ src/taller10_ejercicios/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller10_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Persona {
+    
+      public void presentarse() {
+        System.out.println("Hola, soy una persona.");
+    }
+      
+}
