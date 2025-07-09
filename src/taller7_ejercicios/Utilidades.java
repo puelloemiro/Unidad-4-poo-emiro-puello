d3ad75d359a4fc509b7a9baf0d37f8f9cd8da06f
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:12:53 -0500

    feat: Emiro Puello - Utilidades.java - ejercicio taller 7

---
+++ src/taller7_ejercicios/Utilidades.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller7_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Utilidades {
+    
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: no se puede dividir por cero.");
+            return 0;
+        }
+        return a / b;
+    }
+    
+}
