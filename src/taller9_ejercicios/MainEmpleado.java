2c543021d35a5887bd0affd9bc8f71dfd800e27f
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:04:47 -0500

    feat: Emiro Puello - MainEmpleado.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/MainEmpleado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainEmpleado {
+
+    public static void main(String[] args) {
+    Empleado emp = new Empleado("Camilo", 30, "Tecnología");
+    emp.mostrarDetalles(); // Llama al método sobrescrito
+    }
+    
+}
