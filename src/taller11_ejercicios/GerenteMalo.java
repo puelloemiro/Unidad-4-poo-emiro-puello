96131ce8b5defae813108b6b8a3501075f76c703
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:37:16 -0500

    feat: Emiro Puello - GerenteMalo.java - ejercicio taller 11

---
+++ src/taller11_ejercicios/GerenteMalo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class GerenteMalo extends EmpleadoMalo {
+    
+    public GerenteMalo(String nombre) {
+        super(nombre);
+        //No se sobrescribe calcularSalario(), aunque debería
+    }    
+}
