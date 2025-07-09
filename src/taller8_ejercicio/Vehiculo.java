efde73e0bd5258d6e9f4e6cea61f0312a68a8b83
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:41:00 -0500

    feat: Emiro Puello - Vehiculo.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/Vehiculo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Vehiculo {
+    
+    protected String marca;
+    protected double velocidadMaxima;
+
+    public Vehiculo(String marca, double velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
+    }
+}
