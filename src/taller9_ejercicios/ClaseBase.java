71101df8423bf7d578c1d8c5d8bbe651173fd5a9
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:53:50 -0500

    feat: Emiro Puello - ClaseBase.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/ClaseBase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class ClaseBase {
+    
+      private String mensaje = "Este es un atributo privado";
+
+    public String getMensaje() {
+        return mensaje;
+    }
+    
+}
