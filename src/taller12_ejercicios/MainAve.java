83df14e0f5e29c4232f5cb16e9711d39a57e5982
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:42:18 -0500

    feat: Emiro Puello - MainAve.java - ejercicio taller 12

---
+++ src/taller12_ejercicios/MainAve.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller12_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainAve {
+    
+     public static void main(String[] args) {
+        Ave ave = new Ave();
+
+        ave.volar();   // Método de la interfaz Volador
+        ave.cantar();  // Método de la interfaz Cantante
+        
+    }
+}
