cb6530ddae1cd5ade0953a926aa89397477ee2fb
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:40:11 -0500

    feat: Emiro Puello - MainErrores.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/MainErrores.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainErrores {
+    
+    public static void main(String[] args) {
+        SubClase obj = new SubClase();
+        obj.mostrarMensaje(); // Funciona usando el método público
+    }
+}
