b567c797df6b2aa0671ae1b30b76b48013492be5
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:39:55 -0500

    feat: Emiro Puello - MainCoche.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/MainCoche.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainCoche {
+    
+     public static void main(String[] args) {
+        Coche coche = new Coche("Toyota", 220, 4);
+        coche.mostrarInformacion();
+    }
+     
+}
