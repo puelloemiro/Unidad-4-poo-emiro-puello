6b59cb9a32b7f64a8c00eb33910d8b5700b62d74
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:39:52 -0500

    feat: Emiro Puello - Animal.java - ejercicio taller 13

---
+++ src/taller13_ejercicios/Animal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller13_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ 
+public class Animal implements Volador {  
+    
+/*
+Esta clase implementa la interfaz Volador,
+pero NO proporciona la implementación del método volar().
+
+Error esperado:
+"The type Animal must implement the inherited abstract method Volador.volar()"
+
+Si una clase concreta implementa una interfaz, está obligada a implementar todos sus métodos.
+
+    //FALTA implementar el método 'volar()' 
+}
+*/
