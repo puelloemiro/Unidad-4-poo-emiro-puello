df933f4eae377ed78d331936a1d65da2297a39b0
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:49:40 -0500

    feat: Emiro Puello - ProductoCorrecto.java - ejercicio taller 15

---
+++ src/taller15_ejercicios/ProductoCorrecto.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller15_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class ProductoCorrecto {
+    
+    private String nombre;
+    private double precioBase;
+    private double impuesto;
+
+    public ProductoCorrecto(String nombre, double precioBase, double impuesto) {
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.impuesto = impuesto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public double getImpuesto() {
+        return impuesto;
+    }
+    
+}
