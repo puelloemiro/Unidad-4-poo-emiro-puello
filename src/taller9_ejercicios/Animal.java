3eb74e6aaddcbc6ec584ea5d6cf448dfce6da8e3
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:53:42 -0500

    feat: Emiro Puello - Animal.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/Animal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Animal {
+    
+    protected String especie;
+
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+
+    public void mostrarEspecie() {
+        System.out.println("Especie: " + especie);
+    }
+    
+}
