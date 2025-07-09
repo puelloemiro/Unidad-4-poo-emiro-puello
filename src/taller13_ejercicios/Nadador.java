a2df89b756d0f1ebf43331a960cdbe65fcf890c6
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:40:22 -0500

    feat: Emiro Puello - Nadador.java - ejercicio taller 13

---
+++ src/taller13_ejercicios/Nadador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package taller13_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public interface Nadador {
+    
+    void nadar();
+}
