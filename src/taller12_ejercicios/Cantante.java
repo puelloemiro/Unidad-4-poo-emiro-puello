218d16fd59a2932fb4f291ed896edeb3fd8190db
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:42:11 -0500

    feat: Emiro Puello - Cantante.java - ejercicio taller 12

---
+++ src/taller12_ejercicios/Cantante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package taller12_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public interface Cantante {
+    
+    void cantar();
+    
+}
