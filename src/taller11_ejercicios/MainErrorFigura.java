23b5c66ff89a53f6dc6e640c20c5bbdcab118261
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:37:43 -0500

    feat: Emiro Puello - MainErrorFigura.java - ejercicio taller 11

---
+++ src/taller11_ejercicios/MainErrorFigura.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller11_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainErrorFigura {
+ 
+     public static void main(String[] args) {
+        /*
+        ERROR DE COMPILACIÓN:
+        No se puede instanciar una clase abstracta.
+
+        Error esperado:
+        "Cannot instantiate the type FiguraInvalida"
+        */
+
+        // FiguraInvalida figura = new FiguraInvalida(); //Esto da error si se descomenta
+    }
+}
