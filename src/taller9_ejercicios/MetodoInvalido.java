f8069c96a5c35f81895ea25cf404cf127662c652
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:06:48 -0500

    feat: Emiro Puello - MetodoInvalido.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/MetodoInvalido.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MetodoInvalido {
+    
+    public void metodoInvalido() {
+        /*
+        ERROR:
+        Cannot use 'super' in a class that does not extend another class.
+
+        El uso de 'super' solo es válido dentro de una clase que extiende otra.
+        */
+
+        // System.out.println(super.toString()); // Error de compilación
+    }
+}
+
