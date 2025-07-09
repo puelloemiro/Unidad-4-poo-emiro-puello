f5aade27e25b9ff9ec8fe0b34bf8bf5706026359
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:43:00 -0500

    feat: Emiro Puello - Estudiante.java - ejercicio taller 14

---
+++ src/taller14_ejercicios/Estudiante.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller14_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Estudiante extends Persona {
+    
+    @Override
+    public void presentarse() {
+        System.out.println("Hola, soy un estudiante.");
+    }
+}
