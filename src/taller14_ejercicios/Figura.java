6f065ad7a494938f45527cd5b86f398fa843eea2
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:43:20 -0500

    feat: Emiro Puello - Figura.java - ejercicio taller 14

---
+++ src/taller14_ejercicios/Figura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller14_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Figura {
+    
+    public void calcularArea() {
+        System.out.println("Calculando área de una figura genérica...");
+    }
+}
