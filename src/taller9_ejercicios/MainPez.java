1b855a96a63c8c9616d3361b92c1ae6f455ed934
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:05:22 -0500

    feat: Emiro Puello - MainPez.java - ejercicio taller 9

---
+++ src/taller9_ejercicios/MainPez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller9_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class MainPez {
+    
+    public static void main(String[] args) {
+        
+    Pez pez = new Pez("Pez Payaso", "Agua Salada");
+    pez.mostrarDetalles();
+    
+    }
+    
+}
