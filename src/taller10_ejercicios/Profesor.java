a9851df0d903547665fc69a51bd0f17046ee8446
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:32:03 -0500

    feat: Emiro Puello - Profesor.java - ejercicio taller 10

---
+++ src/taller10_ejercicios/Profesor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller10_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Profesor extends Persona {
+    
+@Override
+
+    public void presentarse() {
+        
+        System.out.println("Hola, soy profesor.");
+    
+    }    
+}
