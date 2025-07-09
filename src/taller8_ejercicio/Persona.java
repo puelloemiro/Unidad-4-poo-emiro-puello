5cbc0a818604b1dd87778c044993778618337680
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:40:30 -0500

    feat: Emiro Puello - Persona.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/Persona.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Persona {
+    
+    protected String nombre;
+    protected int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad + " años");
+    }
+    
+}
