1c0bad5e4d5357560928ff43b08c617f7a4d8733
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:35:58 -0500

    feat: Emiro Puello - Ejemplo4.java - ejemplo taller 11

---
+++ src/taller11_ejemplos/Ejemplo4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 
+package taller11_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ 
+public class Ejemplo4 {
+    
+    // Clase abstracta
+
+public abstract class Animal {
+public abstract void hacerSonido();
+}
+
+
+// Clase derivada que no sobrescribe el método abstracto
+public class Gato extends Animal {
+// Aunque el método hacerSonido no es sobrescrito, se permite debido a la falta de visibilidad o error lógico
+}
+
+}
+*/
