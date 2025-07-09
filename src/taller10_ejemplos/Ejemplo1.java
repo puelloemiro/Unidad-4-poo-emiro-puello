5561d8a9c19c8355ec8a209003fcef0be8be1313
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:17:33 -0500

    feat: Emiro Puello - Ejemplo1.java - ejemplo taller 10

---
+++ src/taller10_ejemplos/Ejemplo1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller10_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Ejemplo1 {
+    
+    // Clase base
+public class Animal {
+    
+public void hacerSonido() {
+    
+System.out.println("El animal hace un sonido.");
+
+}
+
+}
+
+
+// Clase derivada
+public class Perro extends Animal {
+@Override
+public void hacerSonido() {
+System.out.println("El perro ladra.");
+
+ }
+
+ }
+
+}
