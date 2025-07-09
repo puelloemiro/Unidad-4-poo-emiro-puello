88963f47cf77cfe39318809762b2cfc2e2b79c1f
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:39:35 -0500

    feat: Emiro Puello - Ejemplo3.java - ejemplo taller 13

---
+++ src/taller13_ejemplos/Ejemplo3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller13_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Ejemplo3 {
+    
+    // Definición de una interfaz
+public interface Jugador {
+void jugar();
+}
+}
+
+
+/* Clase incorrecta que no implementa el método jugar
+public class Futbolista implements Jugador {
+// Error de compilación: La clase Futbolista debe implementar el método jugar()
+
+}
+*/
