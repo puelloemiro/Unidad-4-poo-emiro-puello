b1e346bed8a84f34a8adfb8c03359fecb3eb477f
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:43:52 -0500

    feat: Emiro Puello - Perro.java - ejercicio taller 14

---
+++ src/taller14_ejercicios/Perro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller14_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Perro extends Animal {
+    
+    public void moverCola() {
+        System.out.println("El perro está moviendo la cola.");
+    }    
+}
