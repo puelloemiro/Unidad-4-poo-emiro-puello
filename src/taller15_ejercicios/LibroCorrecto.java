8b920a0ba117003269d96d5fc54616dfdbc46eb4
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 21:48:28 -0500

    feat: Emiro Puello - LibroCorrecto.java - ejercicio taller 15

---
+++ src/taller15_ejercicios/LibroCorrecto.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller15_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ */
+public class LibroCorrecto {
+    
+   public static class Libro {
+        private String titulo;
+        private String autor;
+
+        public Libro(String titulo, String autor) {
+            this.titulo = titulo;
+            this.autor = autor;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public String getAutor() {
+            return autor;
+        }
+    }
+}
+
