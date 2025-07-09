98f9077d4a959de7043010401f17a4de04b3721a
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:42:40 -0500

    feat: Emiro Puello - PersonaIncompleta.java - ejercicio taller 12

---
+++ src/taller12_ejercicios/PersonaIncompleta.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller12_ejercicios;
+
+/**
+ *
+ * @author usuario1
+ 
+public class PersonaIncompleta implements Saltador {
+    
+// FALTA EL MÉTODO OBLIGATORIO: void saltar()
+
+    // Esto genera un error de compilación automáticamente.    
+    /*
+Esta clase implementa la interfaz Saltador,
+pero NO proporciona la implementación del método saltar().
+
+Por eso, el compilador mostrará este error:
+"The type PersonaIncompleta must implement the inherited abstract method Saltador.saltar()"
+}
+*/
+
+
+
