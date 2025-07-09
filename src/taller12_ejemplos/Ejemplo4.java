937f8683adbb229b4697266da5d58163d9c10b7a
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 20:41:29 -0500

    feat: Emiro Puello - Ejemplo4.java - ejemplo taller 12

---
+++ src/taller12_ejemplos/Ejemplo4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller12_ejemplos;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Ejemplo4 {
+    
+     // Interfaz Volador
+public interface Volador {
+void volar();
+} 
+
+// Clase incorrecta que no implementa todos los métodos
+//public class Avion implements Volador {
+// Error de compilación: La clase Avion debe implementar el método volar()
+}
+
+//}
