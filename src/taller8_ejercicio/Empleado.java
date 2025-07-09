265b95a7efd7552f0cd201bbff298319e8803fcb
Emiro Puello Del Valle <epuellod1@unicartagena.edu.co>
2025-07-08 19:39:24 -0500

    feat: Emiro Puello - Empleado.java - ejercicio taller 8

---
+++ src/taller8_ejercicio/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller8_ejercicio;
+
+/**
+ *
+ * @author usuario1
+ */
+public class Empleado {
+    
+    protected String nombre;
+    protected double salario;
+
+    public Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Salario: $" + salario);
+    }
+    
+}
